77cc5a801fd6fbd88709d4faec4cd2ebada6da1f
mustafeabdi298 <59968812+mustafeabdi298@users.noreply.github.com>
2021-11-10 21:53:06 +0000

    Tagging upcoming movies as must-watch.

---
+++ src/components/cardIcons/addToPlaylist.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@material-ui/core/IconButton";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import PlaylistIcon from "@material-ui/icons/PlaylistAdd"
+
+const AddToPlaylistIcon = ({ movie }) => {
+  
+    const context = useContext(MoviesContext);
+    const handleAddToPlaylist = (e) =>
+    {
+      e.preventDefault();
+      context.addToMustWatch(movie);
+    };
+  return (
+    <IconButton aria-label="add to playlist" >
+      <PlaylistIcon color="primary" fontSize="large" onClick={handleAddToPlaylist} />
+    </IconButton>
+  );
+};
+
+export default AddToPlaylistIcon;
+
